9bfe71555331016c3be489bebe0c7c3ced796205
jefrypozo <jefrypozo@gmail.com>
2025-06-02 15:07:29 -0400

    build: Minimal working build and run

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import tsconfigPaths from "vite-tsconfig-paths"
+import { configDefaults, defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    exclude: [
+      ...configDefaults.exclude,
+      "**/node_modules/**",
+    ],
+  },
+  plugins: [
+    tsconfigPaths({
+      ignoreConfigErrors: true,
+    }),
+  ],
+})
