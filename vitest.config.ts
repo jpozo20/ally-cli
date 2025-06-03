f32a94251cabbbb58c5aa50144f6a7be6ccc212c
jefrypozo <jefrypozo@gmail.com>
2025-06-03 17:13:11 -0400

    ci: final configuration before starting proper development

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
-import tsconfigPaths from "vite-tsconfig-paths"
-import { configDefaults, defineConfig } from "vitest/config"
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: [
-      ...configDefaults.exclude,
-      "**/node_modules/**",
-    ],
+    exclude: [...configDefaults.exclude, '**/node_modules/**'],
   },
   plugins: [
     tsconfigPaths({
