577fb24600f586e65445083158b0980e86b4d318
jefrypozo <jefrypozo@gmail.com>
2025-07-04 00:10:45 -0400

    test: add tests for list and remove commands

--- tests/commands/projectCommand.test.ts
+++ tests/commands/projectCommand.test.ts
@@ -1,11 +1,10 @@
-// @ts-nocheck
-
-import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { describe, it, expect, vi, beforeEach, afterAll } from 'vitest';
 
 import initializeCLI from '@/program/ally-program.js';
 import dataManager from '@/data/data-manager.js';
 
 import * as utils from '@test/test-utils.js';
+import * as logger from '@/utils/messages.js';
 
 // Mock the file manager to avoid actual file operations
 // This is necessary to prevent the CLI from trying to read/write files during tests
@@ -13,17 +12,29 @@ vi.mock('@/data/file-manager.js', async () => {
   const actual = await vi.importActual('@/data/file-manager.js');
   return {
     default: {
+      //@ts-ignore This is a workaround to avoid type errors with mocks
       ...actual.default,
-      saveData: (data: string) => true,
+      saveData: () => true,
+      loadData: () => ({ projects: [] }),
     },
   };
 });
 
 beforeEach(() => {
-  dataManager.resetDatabase();
+  dataManager.getInstance().resetDatabase();
 });
 
 describe('add project', async () => {
+  //Spy on process.exit and replace its implementation
+  const mockExit = vi.spyOn(process, 'exit').mockImplementation((number) => {
+    throw new Error(`process.exit: ${number}`);
+  });
+
+  afterAll(() => {
+    //Restore the original implementation after all tests
+    mockExit.mockRestore();
+  });
+
   const getProgram = async (extraArgs: string[] = [], exitOverride?: boolean) => {
     const program = await initializeCLI();
     if (exitOverride) program.exitOverride();
@@ -34,8 +45,10 @@ describe('add project', async () => {
 
   it('should add the project to the database', async () => {
     await getProgram();
+    const manager = dataManager.getInstance();
 
-    const projects = await dataManager.getProjects();
+    console.log('DataManager instance:', manager);
+    const projects = await manager.getProjects();
     expect(projects.length).toBeGreaterThan(0);
   });
 
@@ -43,8 +56,9 @@ describe('add project', async () => {
     const projectName = 'test-project';
     await getProgram(['--name', projectName]);
 
-    console.log(dataManager);
-    const project = (await dataManager.getProjects())[0];
+    const manager = dataManager.getInstance();
+    console.log(manager);
+    const project = (await manager.getProjects())[0];
     expect(project.name).toEqual(projectName);
   });
 
@@ -52,34 +66,31 @@ describe('add project', async () => {
     const projectPath = '/test/path';
     await getProgram(['--path', projectPath]);
 
-    const project = (await dataManager.getProjects())[0];
+    const manager = dataManager.getInstance();
+    const project = (await manager.getProjects())[0];
     expect(project.path).toEqual(projectPath);
   });
 
   it('should log error if project name already exists', async () => {
-    const consoleSpy = vi.spyOn(console, 'log');
-    const exitSpy = vi.spyOn(process, 'exit').mockImplementation((code) => {
-      return;
-    });
-
     const manager = dataManager.getInstance();
     const projectName = 'existing-project';
     const existingProject = { name: projectName, path: '/existing/path' };
     manager.addProject(existingProject);
 
     const errorMessage = `Project with name "${projectName}" already exists.`;
-    await getProgram(['--name', projectName], true);
+    const fun = getProgram(['--name', projectName], true);
 
-    expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
-    consoleSpy.mockClear();
-    exitSpy.mockClear();
+    await expect(fun).rejects.toThrow(errorMessage);
+
+    mockExit.mockRestore();
   });
 
   it('when no options are passed, it should set the project name to the current dir', async () => {
     await getProgram();
 
+    const manager = dataManager.getInstance();
     const currentDir = utils.getCurrentDir();
-    const project = (await dataManager.getProjects())[0];
+    const project = (await manager.getProjects())[0];
 
     expect(project.name).toEqual(currentDir);
   });
@@ -87,9 +98,63 @@ describe('add project', async () => {
   it('when no options are passed, it should set the project directory to the current dir', async () => {
     await getProgram();
 
+    const manager = dataManager.getInstance();
     const currentDir = process.cwd();
-    const project = (await dataManager.getProjects())[0];
+    const project = (await manager.getProjects())[0];
 
     expect(project.path).toEqual(currentDir);
+  });
+});
+
+describe('list projects', async () => {
+  const getProgram = async (extraArgs: string[] = [], exitOverride?: boolean) => {
+    const program = await initializeCLI();
+    if (exitOverride) program.exitOverride();
+
+    await program.parseAsync(['p', 'list', ...extraArgs], { from: 'user' });
+    return program;
+  };
+
+  it('should list saved projects', async () => {
+    const consoleSpy = vi.spyOn(logger, 'logOutput');
+
+    const projectCount = 3;
+    const projects = utils.getRandomProjects(projectCount);
+
+    const manager = dataManager.getInstance();
+    projects.forEach((project) => manager.addProject(project));
+
+    await getProgram();
+
+    const header = 'Projects names:';
+    const hasHeader = consoleSpy.mock.calls.some((call) => call[0].includes(header));
+    const hasProjects = projects.every((project) => consoleSpy.mock.calls.some((call) => call[0].includes(project.name)));
+
+    const callCount = consoleSpy.mock.calls.length;
+    expect(callCount).toBeGreaterThanOrEqual(projectCount + 1);
+    expect(hasHeader).toBeTruthy();
+    expect(hasProjects).toBeTruthy();
+
+    consoleSpy.mockRestore();
+  });
+
+  describe('remove project', () => {
+    const getProgram = async (extraArgs: string[] = [], exitOverride?: boolean) => {
+      const program = await initializeCLI();
+      if (exitOverride) program.exitOverride();
+
+      await program.parseAsync(['p', 'remove', ...extraArgs], { from: 'user' });
+      return program;
+    };
+
+    it('should log error if no project name is given', async () => {});
+
+    it('should remove given project', async () => {
+      const projectCount = 3;
+      const projects = utils.getRandomProjects(projectCount);
+
+      const manager = dataManager.getInstance();
+      projects.forEach((project) => manager.addProject(project));
+    });
   });
 });
--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -1,4 +1,23 @@
-export function getCurrentDir() {
+import { Project } from '@/data/models/models.js';
+import { faker } from '@faker-js/faker';
+// 
+
+
+export function getCurrentDir(): string | undefined {
   const proccessDir = process.cwd();
   return proccessDir.split('/').pop();
+}
+
+export function getRandomProjects(count: number = 1): Project[] {
+  const projects = [];
+  for (let i = 0; i < count; i++) {
+    const randomName = faker.word.noun();
+    const randomName2 = faker.word.noun();
+    projects.push({
+      name: `${randomName}-${randomName2}`,
+      path: faker.system.directoryPath(),
+    });
+  }
+
+  return projects;
 }
